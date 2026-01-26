3f535e8574aa9b9e33e3728d87bf12f4f92782ba
Yoro1836 <ijaeyong46@outlook.kr>
2026-01-26 14:26:26 +0900

    fix: remove duplicated -ZeroX localversion string

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ function setup_env() {
     export TARGET_BOARD_PLATFORM=gki
     
     # Kernel Version Customization
-    export LOCALVERSION="-ZeroX${VERSION_SUFFIX}"
+    export LOCALVERSION="$VERSION_SUFFIX"
 
     export ANDROID_PRODUCT_OUT=${ANDROID_BUILD_TOP}/out/target/product/${MODEL}
     export OUT_DIR=${ANDROID_BUILD_TOP}/out/msm-${CHIPSET_NAME}-${CHIPSET_NAME}-${TARGET_PRODUCT}
